77567cc6e13f64010ba0017753cb5fbe12289475
unknown <diegomanuelcaca@gmail.com>
2024-07-30 19:23:22 -0500

    backend corregido 1.3

--- src/main.js
+++ src/main.js
@@ -14,7 +14,7 @@ import Swal from 'sweetalert2';
 import { useIntervalStore } from './store/interval.js';
 
 // axios.defaults.baseURL = "http://localhost:4000/";
-axios.defaults.baseURL = "https://backend-gym-proy.onrender.com";
+axios.defaults.baseURL = "https://backend-gym-ikf1.onrender.com";
 
 // sin interval
 // let requestsPending = 0;
